5de1cb534c82dc08fbe0b7ae6c8d0dd9f11152f4
zaynabali-12345 <zaynabali00786@gmail.com>
2025-05-06 15:21:02 +0530

    message

---
+++ BasicOperations.java
@@ -0,0 +1,14 @@
+package BasicOperations;
+
+public class BasicOperations {
+    public static void main(String [] args){
+        int a = 15;
+        int b = 5;
+        int sum = a+b;
+        int diff = a-b;
+        int prod = a*b;
+        System.out.println("Addition:"+sum);
+        System.out.println("Difference:"+diff);
+        System.out.println("Product:"+prod);
+    }
+}
